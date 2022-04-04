a015cc911c6dbdc07458b957bc275c2b83cf8692
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2022-04-04 00:23:06 -0300

    Revert "db heroku"
    This reverts commit bc87851d8c3f0230194a658de791b7e60dbc66df.

--- api/prisma/migrations/20220401150011_init/migration.sql
+++ api/prisma/migrations/20220401150011_init/migration.sql
@@ -11,7 +11,6 @@ CREATE TABLE "Todo" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "folderId" INTEGER,
-    "done" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
 );
